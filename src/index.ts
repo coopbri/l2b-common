dfc3bde1660f8ec377df9bd81d4681b8486991b0
Brian Cooper <brian@brian-cooper.com>
2023-01-25 01:07:40 -0600

    refactor: replace custom user data interface with global namespace augmentation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
+/**
+ * Extend user metadata.
+ * @see {@link https://github.com/clerkinc/javascript/blob/86f68940054ae1add2f73b365c47472ce3f5fd1a/packages/types/src/user.ts}
+ */
+declare global {
+  interface UserPublicMetadata {
+    role: string;
+    [k: string]: unknown;
+  }
+}
+
 export * from "./lib/data";
 export * from "./lib/types";
--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,5 +1,1 @@
-// NB: https://github.com/vitejs/vite/issues/731#issuecomment-678625456
-import type { default as UserData } from "./UserData.interface";
-
 export { default as Role } from "./Role.enum";
-export { UserData };
